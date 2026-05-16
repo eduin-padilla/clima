29cdcae79eed38bb60584ea89d4c39495014dac0
Edwin Padilla <edwinpadillaorozco1@gmail.com>
2026-05-16 11:26:08 -0500

    Proyecto lector de clima

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.css"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className={styles.title} >klk</h1>
+      <p className={styles.container}>1</p>
+      <p className={styles.container}>2</p>
+    </>
+  )
+}
+
+export default App
